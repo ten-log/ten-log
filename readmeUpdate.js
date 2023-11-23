a95281c367e43e0f103454ec45a5950c41e9574e
ten-log <qlqjs674@naver.com>
2023-11-23 10:56:24 +0900

    Fix: readmeUpdate 내용변경

--- readmeUpdate.js
+++ readmeUpdate.js
@@ -5,18 +5,7 @@ import Parser from "rss-parser";
  * README.MD에 작성될 페이지 텍스트
  * @type {string}
  */
-let text = `# Hi there 👋
-
-## 이런 환경에  익숙해요✍🏼
-
-## 언어
-
-<p>
-  <img alt="" src= "https://img.shields.io/badge/JavaScript-F7DF1E?style=flat-square&logo=JavaScript&logoColor=white"/> 
-  <img alt="" src= "https://img.shields.io/badge/TypeScript-black?logo=typescript&logoColor=blue"/>
-</p>
-
-
+let text = `# 👋 Hi there 
 
 <a href="https://github.com/ten-log">
   <img align="center" src="https://github-readme-stats-theta-gules-17.vercel.app/api?username=ten-log&show_icons=true&theme=dark&line_height=20"/>
@@ -28,16 +17,11 @@ let text = `# Hi there 👋
 <br />
 <div>
 <img src="https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white" alt="TS and typescript"/>
-<img src="https://img.shields.io/badge/Jest-C21325?style=for-the-badge&logo=jest&logoColor=white" alt="white jester shoe icon"/> <br/>
-<img src="https://img.shields.io/badge/eslint-3A33D1?style=for-the-badge&logo=eslint&logoColor=white" alt="es lint"/>
-<img src="https://img.shields.io/badge/prettier-1A2C34?style=for-the-badge&logo=prettier&logoColor=F7BA3E" alt="prettier" />
-<img src="https://img.shields.io/badge/npm-CB3837?style=for-the-badge&logo=npm&logoColor=white" alt="npm"/>
+<img src="https://img.shields.io/badge/Express.js-404D59?style=for-the-badge" alt="white jester shoe icon"/> <br/>
+<img src="https://img.shields.io/badge/Java-ED8B00?style=for-the-badge&logo=openjdk&logoColor=white" alt="java"/>
+<img src="https://img.shields.io/badge/Spring-6DB33F?style=for-the-badge&logo=spring&logoColor=white" alt="spring"/>
 </div>
 <br />
-
-
-## Contact me
-
 ## 📕 Latest Blog Posts
 
 `;
@@ -54,12 +38,12 @@ const parser = new Parser({
   const feed = await parser.parseURL("https://yeolceo.tistory.com/rss");
 
   // 최신 5개의 글의 제목과 링크를 가져온 후 text에 추가
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 7; i++) {
     const { title, link } = feed.items[i];
     console.log(`${i + 1}번째 게시물`);
     console.log(`추가될 제목: ${title}`);
     console.log(`추가될 링크: ${link}`);
-    text += `<a href=${link}>${title}</a></br>`;
+    text += `${i + 1} <a href=${link}>${title}</a></br>`;
   }
 
   // README.md 파일 작성
